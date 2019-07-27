d6a181a65e922f294c2872f80254e723f3bdc4f2
kevinmartinda <kevin36.k3@gmail.com>
2019-07-27 12:48:53 +0700

    soalNo3 done

---
+++ soalNo3.js
@@ -0,0 +1,18 @@
+const segitigaSikuSiku = (param) => {
+    let tempData = []
+    let data = []
+        for (let i = 0; i < param; i++) {
+            for (let j = 0; j < param; j++) {
+                if (j === param - i - 1) {
+                    tempData.push('*')
+                    data.push(...tempData)
+                } else {
+                    data.push(' ')
+                }
+            }
+            console.log(...data)
+            data = []
+        }
+}
+
+segitigaSikuSikuMirrored(5)
